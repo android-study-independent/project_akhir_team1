162a5a7572511cd7e4f9cfd237eae4014c3b97a7
Muhammad Adrian Ronaldy <ronaladrian9@gmail.com>
2023-11-06 21:44:19 +0700

    design system setup, include (bottom navigatio)

--- app/src/main/java/com/example/tanify/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/tanify/ui/register/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.example.tanify
+package com.example.tanify.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tanify.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
