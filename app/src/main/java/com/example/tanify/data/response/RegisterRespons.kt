490e73a1b7253f1f8e493caf8a377a07e32ad07b
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-11-12 21:54:38 +0700

    memperbaiki api respon register

--- app/src/main/java/com/example/tanify/data/response/RegisterRespons.kt
+++ app/src/main/java/com/example/tanify/data/response/RegisterRespons.kt
@@ -5,7 +5,49 @@ import com.google.gson.annotations.SerializedName
 data class RegisterRespons(
 	@SerializedName("msg")
 	val message: String? = null,
+	@SerializedName("data")
+	val data: data? = null,
+	@SerializedName("error")
+	val error: error? = null,
 
 )
+
+data class data(
+	@SerializedName("id")
+	val id: String? = null,
+	@SerializedName("nama")
+	val nama: String? = null,
+	@SerializedName("email")
+	val email: String? = null,
+
+)
+
+data class error(
+	@SerializedName("nama")
+	val nama: errordata? = null,
+	@SerializedName("email")
+	val email: errordata? = null,
+	@SerializedName("password")
+	val password: errordata? = null,
+	@SerializedName("konfirmasiPassword")
+	val konfirmasiPassword: errordata? = null,
+
+)
+
+data class errordata(
+	@SerializedName("type")
+	val type: String? = null,
+	@SerializedName("value")
+	val value: String? = null,
+	@SerializedName("msg")
+	val msg: String? = null,
+	@SerializedName("path")
+	val path: String? = null,
+	@SerializedName("location")
+	val location: String? = null,
+
+)
+
+
 
 
