9b84b5bdc6d8e6d050837b34d642f39cb627a427
Muhammad Adrian Ronaldy <ronaladrian9@gmail.com>
2023-11-16 01:12:53 +0700

    add activity: artikel, detail artikel, edit profile, change password

---
+++ app/src/main/java/com/example/tanify/ui/artikel/DetailArtikelActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanify.ui.artikel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tanify.R
+
+class DetailArtikelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_artikel)
+    }
+}
---
+++ app/src/main/java/com/example/tanify/ui/bottomNav/profile/editProfile/ChangeProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanify.ui.bottomNav.profile.editProfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tanify.R
+
+class ChangeProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_profile)
+    }
+}
