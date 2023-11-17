4ed42e50c2029cb88a70edce58b46ba7546a5016
Muhammad Adrian Ronaldy <ronaladrian9@gmail.com>
2023-11-17 14:10:02 +0700

    add weather activity

---
+++ app/src/main/java/com/example/tanify/ui/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanify.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tanify.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
