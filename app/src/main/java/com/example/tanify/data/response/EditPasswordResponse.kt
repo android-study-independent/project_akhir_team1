875dba66ec961a02a7c7b84e9aaae59a6b6549c5
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-11-29 00:06:56 +0700

    memnambah respon edit

--- app/src/main/java/com/example/tanify/data/response/EditPasswordResponse.kt
+++ app/src/main/java/com/example/tanify/data/response/EditPasswordResponse.kt
@@ -1,4 +1,19 @@
 package com.example.tanify.data.response
 
-class EditPassword {
-}
+import com.google.gson.annotations.SerializedName
+
+data class EditPasswordResponse (
+    @SerializedName("msg")
+    val message: String? = null,
+    @SerializedName("errors")
+    val error: errorEditPassword? = null,
+)
+
+data class errorEditPassword(
+    @SerializedName("oldPassword")
+    val oldPassword: errordata? = null,
+    @SerializedName("newPassword")
+    val newPassword: errordata? = null,
+    @SerializedName("confirmPassword")
+    val confirmPassword: errordata? = null,
+)
--- app/src/main/java/com/example/tanify/data/response/EditProfilResponse.kt
+++ app/src/main/java/com/example/tanify/data/response/EditProfilResponse.kt
@@ -1,3 +1,10 @@
 package com.example.tanify.data.response
 
-data class EditProfilResponse()
+import com.google.gson.annotations.SerializedName
+
+data class EditProfilResponse(
+    @SerializedName("msg")
+    val msg: String? = null,
+    @SerializedName("error")
+    val error: errordata? = null,
+)
