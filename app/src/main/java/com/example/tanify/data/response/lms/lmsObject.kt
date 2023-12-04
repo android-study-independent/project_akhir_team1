5f474c1d9bd52b8a9000936c990e3b03181a03b8
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-12-05 06:32:05 +0700

    menambah object untuk respon section, pada get lesson by id

--- app/src/main/java/com/example/tanify/data/response/lms/lmsObject.kt
+++ app/src/main/java/com/example/tanify/data/response/lms/lmsObject.kt
@@ -1,4 +1,47 @@
 package com.example.tanify.data.response.lms
 
-class lmsObject {
-}
+import com.google.gson.annotations.SerializedName
+
+data class ModulItem(
+
+    @field:SerializedName("cover")
+    val cover: String,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("view")
+    val view: Int,
+
+    @field:SerializedName("section")
+    val section: Int,
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("title")
+    val title: String
+)
+
+data class UserLms(
+
+    @field:SerializedName("nama")
+    val nama: String,
+
+    @field:SerializedName("photo")
+    val photo: String
+)
+
+data class SectionItem(
+
+    @field:SerializedName("id_section")
+    val id_section: String,
+
+    @field:SerializedName("section")
+    val title: String,
+
+    @field:SerializedName("progres")
+    val progres: Boolean
+
+)
+
