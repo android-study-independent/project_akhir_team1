d28d773c728b307cb1a7f4e19a1c3f38254a12b0
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-12-06 23:02:15 +0700

    menambah activity baru

---
+++ app/src/main/java/com/example/tanify/ui/lms/ListSearchLesson/ListSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.tasim.tesss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_search)
+    }
+}
